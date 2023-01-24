0660abc37aea3ec4ba0356e79ee4dda2f2b290fd
Gustavo Rondello <gustavorondello99@outlook.com>
2023-01-24 19:27:07 -0300

    feat: scrape_news implementada

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 from parsel import Selector
 import requests
 import time
+from bs4 import BeautifulSoup
 
 
 # Requisito 1
@@ -37,7 +38,33 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    url = selector.css("link[rel='canonical']::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get().strip()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css("span.author a::text").get()
+    comments_count = len(selector.css("ol.comment-list li").getall())
+    summary = (
+        BeautifulSoup(selector.css("div.entry-content p").get(), "html.parser")
+        .get_text()
+        .strip()
+    )
+    tags = selector.css("a[rel='tag']::text").getall()
+    category = selector.css("span.label::text").get()
+
+    news = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category,
+    }
+
+    return news
 
 
 # Requisito 5
