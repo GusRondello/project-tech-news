9304b438577fb9a9d903856865843ae22169530f
Gustavo Rondello <gustavorondello99@outlook.com>
2023-01-24 20:17:59 -0300

    feat: search_by_category implementada

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -30,4 +30,8 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    category_news = search_news(
+        {"category": {"$regex": category, "$options": "i"}}
+    )
+
+    return [(news["title"], news["url"]) for news in category_news]
